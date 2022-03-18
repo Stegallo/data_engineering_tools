2b778d7e39ddc4f9cac3ddb8dd087806ce6c71bd
Stefano Gallotti <stgallotti@egencia.com>
2022-03-17 17:51:21 -0700

    fix mypy

--- data_engineering_tools/hello_world.py
+++ data_engineering_tools/hello_world.py
@@ -1,12 +1,12 @@
-def greet(subject):
+def greet(subject: str) -> str:
     return "hello " + subject
 
 
-def say_hello():
+def say_hello() -> None:
     print(greet("world"))
 
 
-def main():
+def main() -> None:
     say_hello()
 
 
